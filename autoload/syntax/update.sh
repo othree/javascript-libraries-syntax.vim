38fe28fcc75ca80b4a2d234db31ad98507c75c93
othree <othree@gmail.com>
2013-04-21 13:49:27 +0800

    Fix requirejs name in update.sh

--- autoload/syntax/update.sh
+++ autoload/syntax/update.sh
@@ -1,7 +1,7 @@
 
 for filetype in 'coffee' 'ls' 'typescript'
 do
-    for lib in 'jquery' 'underscore' 'backbone' 'prelude' 'angularjs' 'require'
+    for lib in 'jquery' 'underscore' 'backbone' 'prelude' 'angularjs' 'requirejs'
     do
     sed "s/javascript/$filetype/g" $lib.javascript.vim > $lib.$filetype.vim
     done
